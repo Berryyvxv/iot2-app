5642e1c15df7b79c23f3572ade8f1f30f13ffbce
my name <heybassant@gmail.com>
2023-12-09 16:45:49 +0300

    - server adjustments -

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,37 +1,44 @@
-from flask import Flask, Response, render_template, request
-from utils import announcer, format_sse
+from flask import Flask
+
 
 app = Flask(__name__)
-
-@app.route("/")
-def Home():
-    render_template('index.js')
-
-
-@app.route('/listen', methods=['GET'])
-def listen():
-
-    def stream():
-        messages = announcer.listen()  
-        while True:
-            msg = messages.get()  
-            yield msg
-
-    return Response(stream(), mimetype='text/event-stream')
-
-
+# Route for seeing a data
 @app.route('/noiseLevel')
-def noiseLevel():
-    msg = format_sse(data={
-        'tableID': request.args.get('tableID', 1),
-        'noiseLevel': request.args.get('noiseLevel', 'First'),
-        'tableAvailability': request.args.get('tableAvailability', 'Not Available')
-    })
-    announcer.announce(msg=msg)
-    return {}, 200
+def get_noiseLevel():
+    # Returning an api for showing in  reactjs
+    return {
+        'tableID':1, 
+        "noiseLevel":"First",
+        "tableAvailability":"Not Available", 
+        }
 
 
+# @app.route("/")
+# def Home():
+#     render_template('index.js')
 
+
+# @app.route('/listen', methods=['GET'])
+# def listen():
+
+#     def stream():
+#         messages = announcer.listen()  
+#         while True:
+#             msg = messages.get()  
+#             yield msg
+
+#     return Response(stream(), mimetype='text/event-stream')
+
+
+# @app.route('/noiseLevel')
+# def noiseLevel():
+#     msg = format_sse(data={
+#         'tableID': request.args.get('tableID', 1),
+#         'noiseLevel': request.args.get('noiseLevel', 'First'),
+#         'tableAvailability': request.args.get('tableAvailability', 'Not Available')
+#     })
+#     announcer.announce(msg=msg)
+#     return {}, 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
